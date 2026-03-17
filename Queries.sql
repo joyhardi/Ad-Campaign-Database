b804394b4176a55538f31a534c3d24ddf8bf10d4
joyhardi <134623598+joyhardi@users.noreply.github.com>
2026-03-17 16:45:40 +0800

    Add files via upload

---
+++ Queries.sql
@@ -0,0 +1,38 @@
+-- Retrieve all campaigns
+select * from campaign;
+
+-- Retrieve campaigns with a budget below 7000 (in ascending order)
+select * from campaign
+where budget < 7000
+order by budget;
+
+-- Find advertisers and their additional categories
+select AdvertiserName, ac.IndCategory as AdditionalCategories
+from advertiser a left join additionalcategory ac
+on ac.AdvertiserID = a.AdvertiserID;
+
+-- Find advertisers with unpaid bills
+select AdvertiserName, BillDate
+from bill b join advertiser a
+on b.AdvertiserID = a.AdvertiserID
+where b.DateofPayment is null;
+
+-- Number of publishers that advertiser pays to (descending order)
+select a.AdvertiserName, count(distinct pb.PublisherID) as PublisherCount
+from pubbill pb join advertiser a 
+on pb.AdvertiserID = a.AdvertiserID
+group by a.AdvertiserName
+order by publishercount desc;
+
+-- Which advertisers are running camapaigns and what are their budgets
+select a.AdvertiserName, c.CampaignID, c.budget
+from advertiser a join campaign c
+on a.advertiserID = c.advertiserID;
+
+
+
+
+
+
+
+
